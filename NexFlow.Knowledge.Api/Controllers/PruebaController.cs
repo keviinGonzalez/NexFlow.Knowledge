44d7bab554b90ef58e9f1e54e3708b0bf5bd11b5
keviinGonzalez <kevin6734@hotmail.es>
2026-09-01 00:42:51 -0500

    feat: reemplazo del contenedor automaticamente y controlador para pruebas

---
+++ NexFlow.Knowledge.Api/Controllers/PruebaController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NexFlow.Knowledge.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PruebaController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("¡Hola, Mundo!");
+        }
+    }
+}
